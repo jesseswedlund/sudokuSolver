1a249479909bc0a3f092f6f2cc14894394a96614
Jesse Swedlund <swedlundjesse@gmail.com>
2021-01-29 13:52:16 -0800

    first commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+if (process.env.NODE_ENV !== "testing") app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public/index.html"));
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send({ message: err.message });
+});
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+module.exports = app;
